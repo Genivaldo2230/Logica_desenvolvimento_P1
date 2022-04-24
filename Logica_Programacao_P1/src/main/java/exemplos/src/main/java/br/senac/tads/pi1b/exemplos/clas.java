7c0baeb1e60d51f30bbf26b8565a244534d1ea6a
Genivaldo Alves <99696430+Genivaldo2230@users.noreply.github.com>
2022-04-23 21:46:30 -0300

    teste Atalhos do intellij

---
+++ Logica_Programacao_P1/src/main/java/exemplos/src/main/java/br/senac/tads/pi1b/exemplos/clas.java
@@ -0,0 +1,21 @@
+package exemplos.src.main.java.br.senac.tads.pi1b.exemplos;
+
+public class clas {
+
+    private Integer idade;
+
+    public clas(Integer idade) {
+        this.idadesTodos = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "clas{" +
+                "idade=" + idadesTodos +
+                '}';
+    }
+
+    public Integer getIdade() {
+        return idadesTodos;
+    }
+}
---
+++ Logica_Programacao_P1/src/main/java/exemplos/src/main/java/br/senac/tads/pi1b/exemplos/teste.java
@@ -0,0 +1,12 @@
+package exemplos.src.main.java.br.senac.tads.pi1b.exemplos;
+
+import java.util.Arrays;
+
+public class teste {
+    public static void main(String[] args) {
+        Integer idade = new Integer(30);
+        System.out.println("sua idade" + idade);
+
+
+    }
+}
